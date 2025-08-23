e80d900daae0ec2208869a59ac442383172ca4d3
Hristov1846 <Hristov1846@mail.bg>
2025-08-23 16:36:35 +0300

    index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css"; // Tailwind стилове
+import App from "../App"; // ако App.js е в root, иначе "../src/App"
+
+import "./index.css"; // стилове
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
