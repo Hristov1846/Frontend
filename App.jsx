0d85c2b1ba39f3b320e783bb7aee5679e4e9264e
Hristov1846 <Hristov1846@mail.bg>
2025-08-23 16:51:55 +0300

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,28 +1,26 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 // Страници
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
-import Profile from "./pages/Profile.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";   // <- правилното име
 import Marketplace from "./pages/Marketplace.jsx";
 import Wallet from "./pages/Wallet.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="bg-gradient-to-r from-teal-100 to-blue-100 min-h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/marketplace" element={<Marketplace />} />
-          <Route path="/wallet" element={<Wallet />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="bg-gradient-to-r from-teal-400 to-blue-500 min-h-screen">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:id" element={<ProfilePage />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/wallet" element={<Wallet />} />
+      </Routes>
+    </div>
   );
 }
 
