49042b1488ddb53142b87841374007a8d9b539bb
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 14:26:55 +0300

    axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,9 @@
-// src/api/axios.js
 import axios from "axios";
 
-// Тук слагаме URL на бекенда от Render
 const instance = axios.create({
-  baseURL: "https://backend-bogb.onrender.com/api", // 👈 Твоят бекенд
-  withCredentials: true, // включено за cookies, ако се ползват
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://backend-bogb.onrender.com/api", // ⚡ твоя бекенд в Render
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
 });
 
 export default instance;
