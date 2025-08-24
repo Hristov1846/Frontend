253fbb9f797a73118cc3ba789023b118d7479c77
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 09:45:15 +0300

    index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles/index.css";
+import { AuthProvider } from "./context/AuthContext";
+
+// ✅ правилен импорт към css
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
