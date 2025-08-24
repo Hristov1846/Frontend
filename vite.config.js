56e4ca17ac7b4911d3a3487d4e02db0f466466a6
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 10:04:55 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",  // ✅ важно за Netlify
+  build: {
+    outDir: "dist"
+  },
   server: {
     port: 3000
   }
