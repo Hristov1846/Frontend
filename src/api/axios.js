e8f7e5756150272e5ce92c38d07014c58142a3b8
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 10:36:12 +0300

    axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-// ⚡️ Backend URL от Render
-const API_URL = "https://backend-bogb.onrender.com/api";
-
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://backend-bogb.onrender.com/api", // ⚡️ твоят бекенд в Render
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
