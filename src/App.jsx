c025060b52fed4fe8db4c16fdb2a6c271f536932
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 10:23:59 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
-import Chat from "./pages/Chat";
-import Wallet from "./pages/Wallet";
-import Assistant from "./pages/Assistant";
-import Live from "./pages/Live";
-import Marketplace from "./pages/Marketplace";
-import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/chat/:id"
-          element={
-            <ProtectedRoute>
-              <Chat />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/wallet"
-          element={
-            <ProtectedRoute>
-              <Wallet />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/assistant"
-          element={
-            <ProtectedRoute>
-              <Assistant />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/live/:id"
-          element={
-            <ProtectedRoute>
-              <Live />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/marketplace"
-          element={
-            <ProtectedRoute>
-              <Marketplace />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute adminOnly={true}>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
