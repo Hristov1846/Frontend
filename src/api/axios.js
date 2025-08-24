693f5b0af6c3b7207831275b209ab9581c5c466f
Hristov1846 <Hristov1846@mail.bg>
2025-08-24 09:03:10 +0300

    axios.js

---
+++ src/api/axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://your-backend.onrender.com/api",
+});
+
+// добавя Bearer token към заявките
+API.interceptors.request.use((req) => {
+  const user = localStorage.getItem("user");
+  if (user) {
+    req.headers.Authorization = `Bearer ${JSON.parse(user).token}`;
+  }
+  return req;
+});
+
+export default API;
